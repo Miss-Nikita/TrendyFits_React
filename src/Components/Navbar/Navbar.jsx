38c19df86c428ea89f00d00145761427e212a3d5
Miss-Nikita <bisennikita770@gmail.com>
2025-06-25 22:04:21 +0530

    .

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,29 +5,29 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar">
+      <div className="navbar flex justify-between px-8 gap-20">
    <Link to="/">
       <div className="logo">
-        <h2>TrendyFits</h2>
+        <h2 className="  underline text-2xl p-6">TrendyFits</h2>
       </div>
    </Link>
-      <div className="links">
-        <ul>
-          <li>Home</li>
-          <li>Product</li>
-          <li>Contact</li>
-          <li>App</li>
+      <div className="links p-32 py-8 flex justify-between ">
+        <ul className="flex uppercase gap-[29px]">
+          <li className="list-none font-[18px] font-semibold  ">Home</li>
+          <li className="list-none font-[18px] font-semibold  ">Product</li>
+          <li className="list-none font-[18px] font-semibold  ">Contact</li>
+          <li className="list-none font-[18px] font-semibold  ">App</li>
         </ul>
       </div>
-      <div className="nav_icon_wrapper">
+      <div className="nav_icon_wrapper p-6 flex ">
         <Link to="/cart">
-          <div className="nav_cart">
-            <BiCart className="nav_icon" />
-            <p>0</p>
+          <div className="nav_cart flex relative cursor-pointer  ">
+            <BiCart className=" nav_icon text-[34px] ml-[35px] " />
+            <p className="bg-red-700 absolute r-[-18px]  h-[20px] w-[20px]  text-white flex justify-center items-center  ">0</p>
           </div>
         </Link>
 
-        <BiUser className="nav_icon" />
+        <BiUser className=" nav_icon text-[34px] ml-[35px]" />
       </div>
       </div>
     </div>
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div>
-          <h1>HomePage</h1>
+          <h1 className='font-extrabold'>HomePage</h1>
     </div>
   )
 }
