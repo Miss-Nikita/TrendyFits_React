d234178546c06b76c73fa1c955161d7d4258e760
Miss-Nikita <bisennikita770@gmail.com>
2025-06-27 22:28:25 +0530

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <div>
       <div className="hero">
-        <div className="hero_content">
-            <div className="hero_left">
+        <div className="hero_content flex px-[100px] g-20 justify-between ">
+            <div className="hero_left max-w-[50%] mt-[120px] ">
                 <h2>Unleash Premium Sound Quality</h2>
                 <p>Whether you're streaming ypur favorite songs , gaming or working from home , our premium headPhone </p>
                 <button className='bg-red-500'>Shop the Collection</button>
