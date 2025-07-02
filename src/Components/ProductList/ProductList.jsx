117c1ae64b86abb98c4a0b24f8047a5e7d48e4b9
Miss-Nikita <bisennikita770@gmail.com>
2025-07-02 22:14:50 +0530

    .

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../ShopContext/ShopContext'
 
 const ProductList = () => {
+  const {products} = useContext(ShopContext)
   return (
     <div>
-          <h1>ProductList</h1>
+          <div className="product_list">
+            <h2>Our Elegant Collection</h2>
+            <div className="product-grid">
+              
+            </div>
+          </div>
     </div>
   )
 }
