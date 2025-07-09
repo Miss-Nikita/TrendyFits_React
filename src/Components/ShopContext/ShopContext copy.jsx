a4be0d8acfc30a5abf1efd35f473534f9e7f825a
Miss-Nikita <bisennikita770@gmail.com>
2025-07-09 23:35:48 +0530

    .

---
+++ src/Components/ShopContext/ShopContext copy.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+export const ShopContext = createContext();
+
+import { productData } from "../../Data";
+
+const ShopContextProvider = ({ children }) => {
+  const [products, setProducts] = useState(productData);
+
+  return (
+    <ShopContext.Provider value={products}>{children} </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
