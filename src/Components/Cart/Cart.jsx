990079f8a5c579590e5c85ad0134e6b6fc99170d
Miss-Nikita <bisennikita770@gmail.com>
2025-07-19 00:05:21 +0530

    refactor: update Cart component to utilize ShopContext; enhance layout and display of cart items

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../ShopContext/ShopContext'
+import { FiTrash2 } from 'react-icons/fi'
 
 const Cart = () => {
+  const {cart , clearCart ,  total, quantity} = useContext(ShopContext)
   return (
     <div>
-      <h1>Cart</h1>
+      <div className="cart-container">
+        <div className="cart-left">
+          <div className="cart">
+            <h1>Shopping</h1>
+            <h1>Item: ({quantity}) </h1>
+            <FiTrash2 onClick={clearCart} className='delete-cart' />
+          </div>
+          <div className="cart"></div>
+        </div>
+      </div>
     </div>
   )
 }
